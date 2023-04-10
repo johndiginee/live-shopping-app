a13ef6ab4ed226af562d1f0188083d9447272dc7
John Diginee <Johndbizz@gmail.com>
2023-04-10 11:04:21 +0100

    Added LiveVideoRequest model

--- live_shopping_backend/live_shopping/models.py
+++ live_shopping_backend/live_shopping/models.py
@@ -1,3 +1,25 @@
+# live_shopping/models.py
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+UserModel = get_user_model()
+
+class LiveVideoRequest(models.Model):
+    """This class store the user's request for a live video session with a support person."""
+    PENDING = "PENDING"
+    ACTIVE = "ACTIVE"
+    DONE = "DONE"
+    STATUS_CHOICES = [
+        (PENDING, "Pending"),
+        (ACTIVE, "Active"),
+        (DONE, "Done"),
+    ]
+    user_email = models.EmailField()
+    user_name = models.CharField(max_length=128)
+    user_dyte_participant_id = models.UUIDField(unique=True)
+    dyte_meeting_id = models.UUIDField(unique=True)
+    support_user = models.ForeignKey(UserModel, on_delete=models.DO_NOTHING, null=True)
+    support_user_dyte_participant_id = models.UUIDField(null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=PENDING)
+    feedback = models.TextField()
+    product = models.JSONField()
