44eac76f54e09c3d25be990d523c52e17d592a47
John Diginee <Johndbizz@gmail.com>
2023-04-10 17:46:30 +0100

    Updated readme file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # A Live Shopping App With React, Django, and Dyte
 
 This is a live shopping experience using Dyte SDK, React, and Django. Dyte video SDK is a powerful real-time communication platform that enables businesses to integrate video, audio, and chat functionalities into their applications.
+
+<img sr="https://res.cloudinary.com/dkezlmzn1/image/upload/v1681145035/Screenshot_2023-04-10_at_5.42.32_PM_j9f1ip.png"/>
+<img sr="https://res.cloudinary.com/dkezlmzn1/image/upload/v1681145031/Screenshot_2023-04-10_at_5.42.50_PM_smzotv.png"/>
--- live_shopping_backend/live_shopping/serializers.py
+++ live_shopping_backend/live_shopping/serializers.py
@@ -9,13 +9,13 @@ class ProductSerializer(serializers.Serializer):
 
 class LiveVideoRequestSerializer(serializers.ModelSerializer):
     """Serializer for LiveVideoRequest Model"""
-	feedback = serializers.CharField(required=False)
-	product = ProductSerializer()
+    feedback = serializers.CharField(required=False)
+    product = ProductSerializer()
 	
-	class Meta:
-		model = LiveVideoRequest
-		fields = "__all__"
-		read_only_fields = [
+    class Meta:
+        model = LiveVideoRequest
+        fields = "__all__"
+        read_only_fields = [
 			"id", "status", "dyte_meeting_id", "support_user",
             "user_dyte_participant_id", "support_user_dyte_participant_id"
 		]
