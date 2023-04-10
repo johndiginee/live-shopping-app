bf0be1da930fb865e5d281557831df50b6840c03
John Diginee <Johndbizz@gmail.com>
2023-04-10 11:08:55 +0100

    Add serializers

---
+++ live_shopping_backend/live_shopping/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from live_shopping.models import LiveVideoRequest
+
+class ProductSerializer(serializers.Serializer):
+    """Serializer for Product Model"""
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    image = serializers.URLField()
+
+class LiveVideoRequestSerializer(serializers.ModelSerializer):
+    """Serializer for LiveVideoRequest Model"""
+	feedback = serializers.CharField(required=False)
+	product = ProductSerializer()
+	
+	class Meta:
+		model = LiveVideoRequest
+		fields = "__all__"
+		read_only_fields = [
+			"id", "status", "dyte_meeting_id", "support_user",
+            "user_dyte_participant_id", "support_user_dyte_participant_id"
+		]
--- live_shopping_backend/live_shopping/views.py
+++ live_shopping_backend/live_shopping/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
+
+from live_shopping.dyte_api_client import DyteAPIClient
+from live_shopping.models import LiveVideoRequest
+from live_shopping.serializers import LiveVideoRequestSerializer
+
+class LiveVideoRequestViewSet(ViewSet):
+	pass
